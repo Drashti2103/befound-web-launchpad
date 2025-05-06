dd1ddac1b260af1e7993236d7d9cf5efae3e8434
Drashti2103 <janvibuddhadevj@gmail.com>
2025-05-06 19:16:38 +0530

    contact fix

--- api/contact.ts
+++ api/contact.ts
@@ -1,7 +1,8 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+/// <reference types="node" />
+
 import nodemailer from 'nodemailer';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
@@ -13,14 +14,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: 'buddhadevdrashti@gmail.com',
-        pass: 'ehwa fwsm fwgh hsgc'
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS
       }
     });
 
     // Email content
     const mailOptions = {
-      from: 'buddhadevdrashti@gmail.com',
+      from: process.env.GMAIL_USER,
       to: 'info@befound.dev',
       subject: `New Contact Form Submission from ${fullName}`,
       html: `
